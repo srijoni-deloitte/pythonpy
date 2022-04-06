b97598eef1495c3cd393e4626c90078e1ed8e742
srijoni-deloitte <srijochakraborty@deloitte.com>
2022-04-06 11:27:00 +0530

    mini_assignment_4

---
+++ python2.py
@@ -0,0 +1,12 @@
+sendInPoly=lambda a,b,c,x:a*(x**2)+b*x+c
+
+
+##taking input 4 times
+x=int(input())
+a=int(input())
+b=int(input())
+c=int(input())
+
+
+print(sendInPoly(a,b,c,x))
+##so sending into ist function ax**2
